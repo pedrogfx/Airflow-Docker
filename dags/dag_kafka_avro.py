8d646a2be9ab89e54bcb6061eb8f9bda4d84484f
Pedro Rodrigues <contact@pedrorod.com>
2022-07-20 21:14:17 -0300

    First Dag

---
+++ dags/dag_kafka_avro.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import sys
+import os
+from airflow.models.variable import Variable
+from os.path import join
+import yaml
+from airflow import DAG
+
+PATH_IMPORT = Variable.get("IMPORT_PATH")
+sys.path.append(PATH_IMPORT)
+
+with open(join(PATH_IMPORT, 'config', 'dag_kafka_avro.yml'), 'r') as file:
+    cfg_ingestion = yaml.safe_load(file.read())
+
+
+def get_values():
+    return True
+
+default_args = {
+    'start_date': datetime(2022, 7, 20, 0, 0),
+    "owner": "de-pedro",
+}
+
+
+dag = DAG(
+    "dag_kafka_avro",
+    default_args=default_args,
+    schedule_interval='*/120 * * * *',
+    tags=['KAFKA','PYSPARK', 'STREAMING'])
