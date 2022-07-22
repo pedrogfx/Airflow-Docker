07445cc333b36d7f673507626bc1e807e00fcd24
Pedro Rodrigues <contact@pedrorod.com>
2022-07-21 21:13:20 -0300

    Update dag_kafka_avro.py

--- dags/dag_kafka_avro.py
+++ dags/dag_kafka_avro.py
@@ -6,9 +6,17 @@ from os.path import join
 import yaml
 from airflow import DAG
 
+DAG_VARS = Variable.get("DAG_KAFKA_AVRO", deserialize_json=True)
 PATH_IMPORT = Variable.get("IMPORT_PATH")
-sys.path.append(PATH_IMPORT)
+APIKEY = DAG_VARS['APIKEY']
+APISECRET = DAG_VARS['APISECRET']
+BOOTSTRAP_SERVER = DAG_VARS['BOOTSTRAP_SERVER']
 
+
+
+
+
+sys.path.append(PATH_IMPORT)
 with open(join(PATH_IMPORT, 'config', 'dag_kafka_avro.yml'), 'r') as file:
     cfg_ingestion = yaml.safe_load(file.read())
 
@@ -20,7 +28,6 @@ default_args = {
     'start_date': datetime(2022, 7, 20, 0, 0),
     "owner": "de-pedro",
 }
-
 
 dag = DAG(
     "dag_kafka_avro",
